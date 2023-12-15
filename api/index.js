cc6aec31ef2657088162733c10ff86bec48c836e
Deniel Bayron <dpbayron@gmail.com>
2023-12-15 13:19:35 +0800

    Connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
-
+// connect mongodb 
+mongoose.connect(process.env.MONGO).then( () => {
+    console.log('Connected to MongoDB');
+})
+.catch( (err) => {
+    console.log(err);
+})
 
 app.listen(port, () => {
     console.log(`Server now runnning on port ${port}`);

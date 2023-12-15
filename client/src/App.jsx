6d44a5265f0838c16348465fcefcc68af5f25f73
Deniel Bayron <dpbayron@gmail.com>
2023-12-15 12:36:01 +0800

    Create Navbar Components and Page Links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
+import { BrowserRouter , Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
 
 function App() {
  
 
   return (
-    <>
-     
-    </>
+<BrowserRouter>
+
+<Navbar />
+<Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/about' element={<About />}></Route>
+      <Route path='/signin' element={<SignIn />}></Route>
+      <Route path='/profile' element={<Profile />}></Route>
+
+</Routes>
+
+
+</BrowserRouter>
   )
 }
 
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+
+
+export default function Navbar() {
+  return (
+    <nav className="bg-slate-200">
+ 
+        <div className=" flex justify-between mx-auto max-w-6xl p-3 items-center ">
+           <Link to={"/"}> <h1 className="text-2xl font-bold"> Mern Auth </h1> </Link>
+
+            <ul className="flex gap-4">
+                <Link to={"/"}><li>Home</li></Link>
+                <Link to={"/about"}><li>About</li></Link>
+                <Link to={"/signin"}><li>SignIn</li></Link>
+            
+            </ul>
+        </div>
+
+
+    </nav>
+  )
+}
